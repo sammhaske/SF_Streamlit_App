2f3d19672ab186ab0599aaa28ae4f26297b009c6
sammhaske <47880412+sammhaske@users.noreply.github.com>
2023-08-13 22:32:06 +0530

    Update streamlit_app.py
    added few more print statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('hello from the other side')
+
+streamlit.header1('hello from the other side')
+
+streamlit.header6('hello from the other side')
+
+streamlit.text('hello from the other side')
+
