352bf349545eeaaa5a99b23b7a1fbac7cee1c0b1
sammhaske <47880412+sammhaske@users.noreply.github.com>
2023-08-13 23:14:17 +0530

    Update streamlit_app.py
    pulled fruits data in civ format from s3 bucket and publishing to app using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit
+import pandas
 
 streamlit.title('my parent\'s new healthy dinner')
-
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list
